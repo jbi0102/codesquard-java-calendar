0da56d6f7864906854b1259cbeedbf04d59e07d7
shooter-jbi <jbi8045@gmail.com>
2019-01-13 00:08:02 +0900

    Update Calendar repeat

--- src/jbi0102/calendar/Calendar.java
+++ src/jbi0102/calendar/Calendar.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printMoth() {
 		System.out.println("Hello, Calendar");
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-----------------");
@@ -12,13 +18,29 @@ public class Calendar {
 		System.out.println("8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-		//숫자를 입력받아 해당하는 최대 일수를 출력하는 프로그램
+	}
+
+	public static void main(String[] args) {
+
+		// 숫자를 입력받아 해당하는 최대 일수를 출력하는 프로그램
 		Scanner sc = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		System.out.println("반복 횟수를 입력하세요 : ");
+		int loopNum = sc.nextInt();
+
+		int[] month = new int[loopNum];
+
 		System.out.print("알고 싶은 달을 입력 : ");
-		int month = Integer.parseInt(sc.next());
-		int[] lastDay = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.println("입력하신 달" + month + "의 마지막 일은 " + lastDay[month - 1]);
+		for (int i = 0; i < loopNum; i++) {
+			month[i] = Integer.parseInt(sc.next());
+		}
+		for (int i = 0; i < loopNum; i++) {
+			System.out.println("입력하신 달 " + month[i] + "월의 마지막 일은 " + cal.getMaxDaysOfMonth(month[i]));
+		}
+		System.out.println("Bye~");
+		
+		
+		sc.close();
 	}
 
 }
